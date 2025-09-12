13c5c2f758d78bd464095b080e935d143b4df47b
701035287 <701035287@2260I-L29-003>
2025-09-12 09:14:05 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Topic_4___Random_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.Write("Enter the minimum value: ");
+            int min = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the maximum value: ");
+            int max = int.Parse(Console.ReadLine());
+
+            Console.Write("Random numbers: ");
+            for (int i = 0; i < 5; i++)
+            {
+                int randomValue = rand.Next(min, max + 1);
+                Console.Write(randomValue + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
